94114e5213e5e2184581b036de6a88f10fb4dab6
Mouhamedoune  Fall <MEDOUNEFALL126@GMAIL.COM>
2023-10-13 20:06:58 +0000

    routage done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, , component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PageComponent } from './page/page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"login" , component:LoginComponent},
+  {path:"page" , component:PageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
